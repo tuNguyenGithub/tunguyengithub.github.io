4d490d4dd6f037c0dd85dcd0e100ab6aaf32fe5e
Hung Vo <hung.vo@egany.com>
2018-10-26 14:52:44 +0700

    thêm kos-vali-keo de test

---
+++ vali-keo-kos_files/VioH78mbAc_.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["d5mtP"]); }
+
+__d("NumberFormatConsts",["NumberFormatConfig"],(function(a,b,c,d,e,f){a={get:function(a){return b("NumberFormatConfig")}};e.exports=a}),null);
